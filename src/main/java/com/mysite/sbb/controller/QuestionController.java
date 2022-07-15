7a7fbadf3e2ab910c9dba0069f6f531fcca6606b
denist2322 <parksomang125@gmail.com>
2022-07-15 13:47:27 +0900

    오늘 내용

--- src/main/java/com/mysite/sbb/controller/QuestionController.java
+++ src/main/java/com/mysite/sbb/controller/QuestionController.java
@@ -6,8 +6,7 @@ import com.mysite.sbb.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,4 +31,16 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "usr/question/question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate(){
+        return "usr/question/question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
+    }
+
 }
--- src/main/java/com/mysite/sbb/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/service/QuestionService.java
@@ -6,6 +6,7 @@ import com.mysite.sbb.util.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +29,14 @@ public class QuestionService {
         else{
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content){
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        questionRepository.save(question);
     }
 
 }

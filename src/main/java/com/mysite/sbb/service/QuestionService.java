6dd9d889265cc5262a6aeb2ea8253654fd2522dc
denist2322 <parksomang125@gmail.com>
2022-07-16 22:06:43 +0900

    작성일시 역순으로

--- src/main/java/com/mysite/sbb/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/service/QuestionService.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,31 +21,32 @@ public class QuestionService {
     @Autowired
     private QuestionRepository questionRepository;
 
-    public Question getQuestion(Integer id){
+    public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
-        if(question.isPresent()){
+        if (question.isPresent()) {
             return question.get();
-        }
-
-        else{
+        } else {
             throw new DataNotFoundException("question not found");
         }
     }
 
-    public void create(String subject, String content){
+    public void create(String subject, String content) {
         Question question = new Question();
         question.setSubject(subject);
         question.setContent(content);
         question.setCreateDate(LocalDateTime.now());
         questionRepository.save(question);
     }
+
     public List<Question> getList() {
         return questionRepository.findAll();
     }
-    
+
     //오버로딩(Overroading)
-    public Page<Question> getList(int page){
-        Pageable pageable = PageRequest.of(page,10);
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 

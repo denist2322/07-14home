f466c5438f56776f5f967627a08b0a38af74c31a
denist2322 <parksomang125@gmail.com>
2022-07-22 18:35:40 +0900

    답변개수 표시

--- src/main/java/com/mysite/sbb/dao/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.sbb.dao;
-
-import com.mysite.sbb.domain.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Integer> {
-}
